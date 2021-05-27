546e7e8edcd1a06fc0cd98fdf37adee31712b1b7
Rinat Kamaletdinov <sky2208@ya.ru>
2021-05-28 00:16:34 +0400

    Done2

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,9 @@ const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+  devServer: {
+    port:9000,
+  },
   optimization: {
     minimizer: [
       new TerserPlugin({}),
